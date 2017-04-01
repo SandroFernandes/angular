22b2074d1db93f7f12b06a9f1e6cd9733e5776d9
Sandro Fernandes <Sandro.Fernandes@SoftRent.com.br>
2017-03-31 21:31:26 -0300

    two way databinds

--- first/src/app/server-teste/server-teste.component.ts
+++ first/src/app/server-teste/server-teste.component.ts
@@ -1,16 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-server-teste',
   templateUrl: './server-teste.component.html',
   styleUrls: ['./server-teste.component.css']
 })
+
+
 export class ServerTesteComponent implements OnInit {
+  allowAddServer = false;
+  serverCreationStatus = "Server was not created !";
+  serverName = 'Teste example';
 
-
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.allowAddServer = true;
+    }, 2000)
+  }
 
   ngOnInit() {
   }
 
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created ! ' + this.serverName
+  }
+
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+    console.log(event);
+  }
 }
